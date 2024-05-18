5d7872bff34d6e7b938bdc8e33ee261821389d17
Leandro Silva <leanddrorrs@outlook.com>
2024-05-18 18:41:17 -0300

    criação da classe SmartTv

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,9 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println("1"+1+"1"+(1+1+1)); //1113
+        System.out.println((1+1+1)+"1"+1+"1"); //3111
+        System.out.println(1+1+"1"+"1"+(1+1+1)); //2113
+        System.out.println("1"+1+1+1+1+1); //111111
     }
 }
