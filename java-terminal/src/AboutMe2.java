9501e82075b659fa5fe00582e9a6fadd2104c4d0
Leandro Silva <leanddrorrs@outlook.com>
2024-05-18 22:36:32 -0300

    update sistema java-terminal

--- java-terminal/src/AboutMe2.java
+++ java-terminal/src/AboutMe2.java
@@ -8,21 +8,21 @@ public class AboutMe2 {
         // Criando o objeto scanner
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.println("Digite seu nome");
+        System.out.println("Digite seu nome:");
         String nome = scanner.next();
 
-        System.out.println("Digite seu sobrenome");
+        System.out.println("Digite seu sobrenome:");
         String sobrenome = scanner.next();
 
-        System.out.println("Digite sua idade");
-        double idade = scanner.nextInt();
+        System.out.println("Digite sua idade:");
+        int idade = scanner.nextInt();
 
-        System.out.println("Digite sua altura");
+        System.out.println("Digite sua altura (em metros):");
         double altura = scanner.nextDouble();
 
         System.out.println("Olá, me chamo " + nome + " " + sobrenome);
         System.out.println("Tenho " + idade + " anos");
-        System.out.println("Minha altura é " + altura + "cm");
+        System.out.println("Minha altura é " + altura + "m");
 
     }
 }
