58c245ca82305f07cf10d4f5c955ce7cbdac86c8
Leandro Silva <leanddrorrs@outlook.com>
2024-07-03 13:31:57 -0300

    Initial commit

---
+++ desafio-api-rest-nuvem/src/main/java/me/leanddrodev/bootcamp_java/BootcampJavaApplication.java
@@ -0,0 +1,13 @@
+package me.leanddrodev.bootcamp_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampJavaApplication.class, args);
+	}
+
+}
---
+++ desafio-api-rest-nuvem/src/test/java/me/leanddrodev/bootcamp_java/BootcampJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.leanddrodev.bootcamp_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
