c66a9eae408479f4b3fe303cfe959e79e9167f9e
Leandro Silva <leanddrorrs@outlook.com>
2024-07-06 13:42:39 -0300

    Desafio api rest nuvem

---
+++ desafio-api-rest-na-nuvem/src/main/java/me/leanddrodev/service/impl/UserServiceImpl.java
@@ -0,0 +1,31 @@
+package me.leanddrodev.service.impl;
+
+import me.leanddrodev.domain.model.User;
+import me.leanddrodev.domain.repository.UserRepository;
+import me.leanddrodev.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public User create(User userToCreate) {
+        if (userRepository.existsByAccountNumber(userToCreate.getAccount().getNumber())) {
+            throw new IllegalArgumentException("This Account number already exists.");
+        }
+        return userRepository.save(userToCreate);
+    }
+}
